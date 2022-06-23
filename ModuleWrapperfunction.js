4690dbd35fdd9b7ee64a9fc5ac08534f2dec87d2
KKOLLANA <kiran-kumar.kollana@capgemini.com>
2022-06-23 20:26:55 +0530

    ModuleWrapperFunction

---
+++ ModuleWrapperfunction.js
@@ -0,0 +1,11 @@
+(function(expotrs, require, module, __filename, __dirname){
+
+    kiran = {
+        name:"Kiran",
+        favnum : 7,
+        Comp : "Capgemini"
+    }
+    console.log(expotrs, require, module, __filename, __dirname)
+    
+    module.exports = kiran;
+})
