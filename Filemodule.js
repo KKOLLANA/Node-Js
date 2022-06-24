64755fabe537b2216ed2801072197b816bec02ca
KKOLLANA <kiran-kumar.kollana@capgemini.com>
2022-06-24 18:06:05 +0530

    intentionallyBlockingMethod

---
+++ Filemodule.js
@@ -0,0 +1,11 @@
+// if We want to stop intentionally 
+
+const fs = require('fs');
+
+
+const a = fs.readFileSync('file.txt')
+    console.log(a.toString())
+
+
+ console.log("Fineshed reading file") // This line is intentionally blocked by using readFileSync method
+ 
