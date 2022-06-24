8e1702898967a2eee507266125f6f0c26557cbf6
KKOLLANA <kiran-kumar.kollana@capgemini.com>
2022-06-24 17:47:42 +0530

    added explanation

--- fsModule.js
+++ fsModule.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 
 fs.readFile('file.txt', 'utf8', (err,data)=>{
-    console.log(err, data)
+    console.log(err, data) 
 })
 
-// const a = fs.readfilesync('file.txt')
-// console.log(a.tostring()) 
-// tostring is used to check the content in the file 
 
 
-console.log("Fineshed reading file")
+ console.log("Fineshed reading file")
+
+//  in this console.log("Fineshed reading file") will print because 
+// fs.readFile('file.txt', 'utf8', (err,data) take some time to reffer and run the output so till then work should not stop thats y it will print. the further data
+// will be executed because it works on non-blocking I/O Module
 
