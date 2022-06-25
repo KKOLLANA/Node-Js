7e8779f081f073cb5c4c4b0450fcc81dfe68f734
KKOLLANA <kiran-kumar.kollana@capgemini.com>
2022-06-25 17:34:01 +0530

    commonjsmodule

---
+++ commonModule1.js
@@ -0,0 +1,7 @@
+//Example of commonjsmodule
+
+function simple(){  //we have Created function here we are going to export this function to commonModule2
+    console.log("Work hard")
+    }
+
+    module.exports = simple
