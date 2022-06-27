037d1510b6c308839d2c86def3a28d5d9f625aa2
KKOLLANA <kiran-kumar.kollana@capgemini.com>
2022-06-27 17:05:56 +0530

    expressmodule

---
+++ expressApp.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port =3000
+app.get('/',(req, res)=>{
+    res.send('hello world')
+    })
+app.get('/about',(req,res)=>{
+    res.send('THis is about page')
+    })
+app.listen(port,()=>{
+    console.log('Example app listening ')
+})
